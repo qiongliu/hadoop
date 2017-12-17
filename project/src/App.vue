a32bd7f578ce6c3932d88bc29f845c0d9b61da45
林一 <273984995@qq.com>
2017-12-17 18:18:37 +0800

    scss

---
+++ project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="app">
+  	<m-top></m-top>
+  	<Carousel 
+  		v-model="carouselIndex"
+  		:loop="loop",
+  		:autoplay="autoPlay",
+  		:autoplay-speed="sutoplaySpeed">
+      <CarouselItem>
+          <div class="demo-carousel">1</div>
+      </CarouselItem>
+      <CarouselItem>
+          <div class="demo-carousel">2</div>
+      </CarouselItem>
+      <CarouselItem>
+          <div class="demo-carousel">3</div>
+      </CarouselItem>
+      <CarouselItem>
+          <div class="demo-carousel">4</div>
+      </CarouselItem>
+    </Carousel>
+	</div>
+</template>
+
+<script>
+	import mTop from 'components/m-top/m-top'
+  export default {
+      components: {
+      	mTop
+      },
+      data () {
+      	return {
+      		carouselIndex: 0,
+      		caarouselSetting: {
+
+      		}
+      	}
+      }
+  }
+</script>
---
+++ project/src/components/m-top/m-top.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="top">
+		<div class="container clearfix">
+			<div class="logo fl">
+				<img src="./1.png" :alt="logoAlt">
+			</div>
+			<ul class="nav fr">
+				<li v-for="(item,index) in navList">
+					<a href="javascript:;" :class="{}">{{item.name}}</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				logoAlt: '',
+				navList: []
+			}
+		},
+		created () {
+			this._getData()
+		},
+		methods: {
+			_getData () {
+				this.logoAlt = '汉城街道综合管理服务平台'
+				this.navList = [
+					{
+						name: '首页',
+						href: ''
+					},
+					{
+						name: '空间',
+						href: ''
+					},
+					{
+						name: '新闻',
+						href: ''
+					},
+					{
+						name: '视频',
+						href: ''
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import "../../common/css/variable";
+
+	.top {
+		height: 60px;
+		line-height: 60px;
+		background-color: $c-bg-top;
+		img {
+			height: 40px;
+			vertical-align: middle;
+		}
+	}
+	.nav {
+		li {
+			display: inline-block;
+		}
+		a {
+			font-size: $fz-md;
+			color: #fff;
+			padding: 0 20px;
+		}
+	}
+</style>
+
+
